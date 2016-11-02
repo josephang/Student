7bd64b8e12bed054d0f1fb98ed7bb9ce9f3bdec9
Joseph Ang <josephang05@gmail.com>
2016-11-01 20:58:03 -0700

    November Update

--- Project4/src/project4/Test.java
+++ Project4/src/project4/Test.java
@@ -47,3 +47,20 @@ public class Test {
     }
 }
 //System.out.println("");
+/*
+	Questions 
+	
+	Which implementation is easier to write? 
+		Personally, I think that my implementation was the easiest to code because
+		I know what I am using and how I am personally implementing the Queue.
+	Which is easier to understand? 
+		I think that a basic understanding must be reached before any type of 
+		comparison can be made. Sure, linked lists are basic, but it only slightly, 
+		if not even differs from a Queue based implementation. It is pretty subjective
+		in my point of view.
+	Which is more efficient?
+		In terms of efficiency, the answer depends on how the list or series of objects
+		will be used. If they are constantly being modified, my implementation is better,
+		if it is just a static readback of information, the implementation in the book is 
+		better.
+*/
